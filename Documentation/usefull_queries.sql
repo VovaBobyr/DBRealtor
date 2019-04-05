3239d8c014df38a523734617198aff89ae61d516
VovaB <vb.subscribe@gmail.com>
2019-04-05 17:57:50 +0200

    Update usefull queries

--- Documentation/usefull_queries.sql
+++ Documentation/usefull_queries.sql
@@ -4,3 +4,12 @@ p.* FROM dbrealtor.byty_prodej AS p WHERE region LIKE '%Praha%'
 AND (title LIKE '%3+kk%' OR title LIKE '%4+kk%' OR title LIKE '%3+1%' OR title LIKE '%4+1%') AND cena !=0
 AND vlastnictvi !='Družstevní' AND cena < 5000000 AND date_close IS NULL)
 
+-- Selection from projects
+-- 3,4 or 2 with condition squ
+SELECT title, cena, poznamka_k_cene, region, subregion, podlazi, uzitna_plocha  FROM projekty_items WHERE
+cena < 6000000 AND
+((title LIKE '%2+kk%' AND CONVERT(REPLACE(SUBSTRING(uzitna_plocha, 1, LOCATE(' m2', uzitna_plocha)),' ', ''), UNSIGNED INTEGER) > 65) OR
+(title LIKE '%2+1%' AND CONVERT(REPLACE(SUBSTRING(uzitna_plocha, 1, LOCATE(' m2', uzitna_plocha)),' ', ''), UNSIGNED INTEGER) > 65) OR
+title LIKE '%3+kk%' OR title LIKE '%3+kk%' OR
+title LIKE '%4+kk%' OR title LIKE '%4+1%')
+ORDER BY cena
